95271043fb872c889155f7d6bd7b78923ce0ff52
Marko Ljubicic <revscare@gmail.com>
2018-10-13 16:22:38 -0600

    Add project files.

---
+++ HovercraftSerComGUI/Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace HovercraftSerComGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.opTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lengthTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.rcvdLabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.rwCheckBox = new System.Windows.Forms.CheckBox();
+            this.regStartTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.Label11 = new System.Windows.Forms.Label();
+            this.sentLabel = new System.Windows.Forms.Label();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.syncMotorBox = new System.Windows.Forms.CheckBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.leftMotorBar = new System.Windows.Forms.TrackBar();
+            this.rightMotorBar = new System.Windows.Forms.TrackBar();
+            this.comBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.leftMotorBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightMotorBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Operation";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(131, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "W/R";
+            // 
+            // opTextBox
+            // 
+            this.opTextBox.Location = new System.Drawing.Point(17, 71);
+            this.opTextBox.Name = "opTextBox";
+            this.opTextBox.Size = new System.Drawing.Size(86, 22);
+            this.opTextBox.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(399, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 25);
+            this.label4.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(202, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 25);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Length";
+            // 
+            // lengthTextBox
+            // 
+            this.lengthTextBox.Location = new System.Drawing.Point(207, 70);
+            this.lengthTextBox.Name = "lengthTextBox";
+            this.lengthTextBox.Size = new System.Drawing.Size(49, 22);
+            this.lengthTextBox.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(417, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 25);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Data";
+            // 
+            // dataTextBox
+            // 
+            this.dataTextBox.Location = new System.Drawing.Point(422, 70);
+            this.dataTextBox.Name = "dataTextBox";
+            this.dataTextBox.Size = new System.Drawing.Size(98, 22);
+            this.dataTextBox.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(4, 311);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 25);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Received";
+            // 
+            // rcvdLabel
+            // 
+            this.rcvdLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.rcvdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rcvdLabel.Location = new System.Drawing.Point(136, 310);
+            this.rcvdLabel.Name = "rcvdLabel";
+            this.rcvdLabel.Size = new System.Drawing.Size(299, 25);
+            this.rcvdLabel.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 121);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 25);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "COM";
+            // 
+            // rwCheckBox
+            // 
+            this.rwCheckBox.AutoSize = true;
+            this.rwCheckBox.Location = new System.Drawing.Point(136, 75);
+            this.rwCheckBox.Name = "rwCheckBox";
+            this.rwCheckBox.Size = new System.Drawing.Size(18, 17);
+            this.rwCheckBox.TabIndex = 21;
+            this.rwCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // regStartTextBox
+            // 
+            this.regStartTextBox.Location = new System.Drawing.Point(302, 70);
+            this.regStartTextBox.Name = "regStartTextBox";
+            this.regStartTextBox.Size = new System.Drawing.Size(49, 22);
+            this.regStartTextBox.TabIndex = 22;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(297, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 25);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Reg Start";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(4, 170);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(109, 25);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Left Motor";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(4, 217);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(122, 25);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Right Motor";
+            // 
+            // Label11
+            // 
+            this.Label11.AutoSize = true;
+            this.Label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label11.Location = new System.Drawing.Point(4, 270);
+            this.Label11.Name = "Label11";
+            this.Label11.Size = new System.Drawing.Size(57, 25);
+            this.Label11.TabIndex = 30;
+            this.Label11.Text = "Sent";
+            // 
+            // sentLabel
+            // 
+            this.sentLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.sentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sentLabel.Location = new System.Drawing.Point(136, 270);
+            this.sentLabel.Name = "sentLabel";
+            this.sentLabel.Size = new System.Drawing.Size(299, 25);
+            this.sentLabel.TabIndex = 31;
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(455, 311);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(75, 23);
+            this.SendButton.TabIndex = 32;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            // 
+            // syncMotorBox
+            // 
+            this.syncMotorBox.AutoSize = true;
+            this.syncMotorBox.Location = new System.Drawing.Point(471, 225);
+            this.syncMotorBox.Name = "syncMotorBox";
+            this.syncMotorBox.Size = new System.Drawing.Size(18, 17);
+            this.syncMotorBox.TabIndex = 33;
+            this.syncMotorBox.UseVisualStyleBackColor = true;
+            this.syncMotorBox.CheckedChanged += new System.EventHandler(this.syncMotorBox_CheckedChanged);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(468, 170);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(41, 18);
+            this.label12.TabIndex = 34;
+            this.label12.Text = "Sync";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(468, 188);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(56, 18);
+            this.label13.TabIndex = 35;
+            this.label13.Text = "Motors";
+            // 
+            // leftMotorBar
+            // 
+            this.leftMotorBar.LargeChange = 32;
+            this.leftMotorBar.Location = new System.Drawing.Point(136, 156);
+            this.leftMotorBar.Maximum = 1024;
+            this.leftMotorBar.Name = "leftMotorBar";
+            this.leftMotorBar.Size = new System.Drawing.Size(313, 56);
+            this.leftMotorBar.SmallChange = 32;
+            this.leftMotorBar.TabIndex = 36;
+            this.leftMotorBar.TickFrequency = 32;
+            this.leftMotorBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.leftMotorBar.Value = 256;
+            // 
+            // rightMotorBar
+            // 
+            this.rightMotorBar.LargeChange = 32;
+            this.rightMotorBar.Location = new System.Drawing.Point(136, 211);
+            this.rightMotorBar.Maximum = 1024;
+            this.rightMotorBar.Name = "rightMotorBar";
+            this.rightMotorBar.Size = new System.Drawing.Size(313, 56);
+            this.rightMotorBar.SmallChange = 32;
+            this.rightMotorBar.TabIndex = 37;
+            this.rightMotorBar.TickFrequency = 32;
+            this.rightMotorBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.rightMotorBar.Value = 256;
+            // 
+            // comBox
+            // 
+            this.comBox.FormattingEnabled = true;
+            this.comBox.Location = new System.Drawing.Point(136, 121);
+            this.comBox.Name = "comBox";
+            this.comBox.Size = new System.Drawing.Size(121, 24);
+            this.comBox.TabIndex = 38;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 345);
+            this.Controls.Add(this.comBox);
+            this.Controls.Add(this.rightMotorBar);
+            this.Controls.Add(this.leftMotorBar);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.syncMotorBox);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.sentLabel);
+            this.Controls.Add(this.Label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.regStartTextBox);
+            this.Controls.Add(this.rwCheckBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.rcvdLabel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dataTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lengthTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.opTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Serial Test";
+            ((System.ComponentModel.ISupportInitialize)(this.leftMotorBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightMotorBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox opTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox lengthTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox dataTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label rcvdLabel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox rwCheckBox;
+        private System.Windows.Forms.TextBox regStartTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label Label11;
+        private System.Windows.Forms.Label sentLabel;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.CheckBox syncMotorBox;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TrackBar leftMotorBar;
+        private System.Windows.Forms.TrackBar rightMotorBar;
+        private System.Windows.Forms.ComboBox comBox;
+    }
+}
+
---
+++ HovercraftSerComGUI/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+using System.IO.Ports;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HovercraftSerComGUI
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+            comBox.Click += new EventHandler(COMbox_Clicked);
+            leftMotorBar.Scroll += new EventHandler(Bar_Scroll);
+            rightMotorBar.Scroll += new EventHandler(Bar_Scroll);
+
+            //Thread newThread = new Thread(ReceiveFrame);
+            //newThread.Start();
+        }
+
+        /// <summary>
+        /// Send a packet across the COM port specified in the COM text box. 
+        /// Checks that the port is valid and open.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            string strData, opStr;
+            bool isPortValid;
+            UInt32 op;
+
+            isPortValid = InitializePort();
+
+            if (isPortValid)
+            {
+                if (!UInt32.TryParse(opTextBox.Text, System.Globalization.NumberStyles.HexNumber, null, out op))
+                    return;
+
+                opStr = opTextBox.Text;
+
+                if (rwCheckBox.Checked)
+                {
+                    op++;
+                    opStr = op.ToString("X");
+                }
+
+                opTextBox.Enabled = false;
+                rwCheckBox.Enabled = false;
+                lengthTextBox.Enabled = false;
+                dataTextBox.Enabled = false;
+
+                if (!serialPort.IsOpen)
+                    serialPort.Open();
+
+                strData = opStr + lengthTextBox.Text + regStartTextBox.Text + dataTextBox.Text;
+
+                TransmitFrame(strData);
+
+                if (serialPort.IsOpen)
+                    serialPort.Close();
+
+                opTextBox.Enabled = true;
+                rwCheckBox.Enabled = true;
+                lengthTextBox.Enabled = true;
+                dataTextBox.Enabled = true;
+            }
+        }
+
+        // Populates the combo text box with available
+        // COM port names.
+        private void COMbox_Clicked(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            comBox.Items.AddRange(ports);
+        }
+
+        private void Bar_Scroll(object sender, EventArgs e)
+        {
+            RunMotors(sender);
+        }
+
+        private void syncMotorBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (syncMotorBox.Checked)
+                rightMotorBar.Value = leftMotorBar.Value;
+        }
+
+        /// <summary>
+        /// Initialize COM port to protocol specification.
+        /// </summary>
+        /// <returns></returns>
+        private bool InitializePort()
+        {
+            bool portValid = false;
+
+            if (comBox.Text == "")
+                return portValid;
+
+            serialPort = new SerialPort
+            {
+                PortName = comBox.Text,
+                BaudRate = 19200,
+                Parity = Parity.None,
+                StopBits = StopBits.One,
+                DataBits = 8,
+                Handshake = Handshake.None,
+                RtsEnable = true,
+
+                ReadTimeout = 500,
+                WriteTimeout = 500
+            };
+
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                if (comBox.Text == s)
+                    portValid = true;
+            }
+
+            return portValid;
+        }
+
+        /// <summary>
+        /// Transmits a frame of the form <0xAA55, strData, 0x55AA>
+        /// </summary>
+        /// <param name="strData"></param>
+        private void TransmitFrame(string strData)
+        {
+            byte[] data;
+
+            if (strData.Length % 2 != 0)
+                strData = strData + "0";
+
+            // Add sync header and footer
+            strData = "AA55" + strData + "55AA";
+
+            // Convert string message into data byte array
+            data = HexStrToByteArray(strData);
+
+            // Set the read/write bit in the OP field
+            /*if (rwCheckBox.Checked)
+                data[2] = (byte)(data[2] | 0x01);
+            else
+                data[2] = (byte)(data[2] & 0xFE);*/
+
+            serialPort.Write(data, 0, data.Length);
+            sentLabel.Text = "0x" + strData;
+        }
+
+        /// <summary>
+        /// Receives a frame of the form <0xAA55, data, 0x55AA>
+        /// </summary>
+        private void ReceiveFrame()
+        {
+            bool read_byte = true;
+            bool read_aa = false;
+            bool read_55 = false;
+            bool header_received = false;
+
+            int next_byte;
+            ArrayList data = new ArrayList();
+
+            while (read_byte)
+            {
+                next_byte = serialPort.ReadByte();
+
+                if (next_byte.ToString("X") == "AA")
+                    read_aa = true;
+
+                if (next_byte.ToString("X") == "55" && read_aa)
+                {
+                    header_received = true;
+                    read_aa = false;
+                }
+
+                if (header_received)
+                {
+                    if (next_byte.ToString("X") == "55")
+                        read_55 = true;
+
+                    if (next_byte.ToString("X") == "AA" && read_55)
+                        read_byte = false;
+                }
+
+                data.Add(next_byte.ToString("X"));
+            }
+
+            this.Invoke(new MethodInvoker(() => rcvdLabel.Text = data.ToString()));
+        }
+
+        private async void RunMotors(object sender)
+        {
+            string strData, op, speed;
+            bool isPortValid;
+            ushort value;
+
+            if (syncMotorBox.Checked)
+            {
+                op = "22"; // sync motors op
+                if (sender == leftMotorBar)
+                {
+                    value = (ushort)leftMotorBar.Value;
+                    rightMotorBar.Value = leftMotorBar.Value;
+                }
+                else
+                {
+                    value = (ushort)rightMotorBar.Value;
+                    leftMotorBar.Value = rightMotorBar.Value;
+                }
+            }
+            else if (sender == leftMotorBar)
+            {
+                op = "24"; // left motor op
+                value = (ushort)leftMotorBar.Value;
+            }
+            else
+            {
+                op = "26"; // right motor op
+                value = (ushort)rightMotorBar.Value;
+            }
+
+            isPortValid = InitializePort();
+
+            if (isPortValid)
+            {
+                opTextBox.Enabled = false;
+                rwCheckBox.Enabled = false;
+                lengthTextBox.Enabled = false;
+                dataTextBox.Enabled = false;
+
+                if (!serialPort.IsOpen)
+                    serialPort.Open();
+
+                speed = value.ToString("X");
+
+                if (speed.Length == 3)
+                {
+                    speed = "0" + speed;
+                }
+                else if (speed.Length == 2)
+                {
+                    speed = "00" + speed;
+                }
+
+                strData = op + "02" + speed;
+
+                TransmitFrame(strData);
+
+                if (serialPort.IsOpen)
+                    serialPort.Close();
+
+                await Task.Delay(10);
+
+                opTextBox.Enabled = true;
+                rwCheckBox.Enabled = true;
+                lengthTextBox.Enabled = true;
+                dataTextBox.Enabled = true;
+            }
+        }
+
+        private byte[] HexStrToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                 .Where(x => x % 2 == 0)
+                 .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                 .ToArray();
+        }
+    }
+}
